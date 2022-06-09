35801b2f1e45a91039cf346f2372c6f4707e6eeb
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-09 13:05:26 -0300

    Primeiro desafio de lógica.

---
+++ script.js
@@ -0,0 +1,10 @@
+let aluno = "Maria"
+let idade = 18
+let anoAtual = 2022
+let subtracao = anoAtual - idade
+console.log(subtracao + idade)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = false
+console.log(typeof quartaFeira)
